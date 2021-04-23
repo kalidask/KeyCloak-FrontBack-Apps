a070347e6602f8eec7eedc0c8fc3fcfa0f4c7ce9
kalidas <kalidas@redhat.com>
2021-04-23 11:42:20 +0530

    sso adapter added

--- frontend-app/src/environments/environment.ts
+++ frontend-app/src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8180', //Spring Boot API
+  apiUrl: 'http://localhost:8280', //Spring Boot API
 
 
   //Keycloak configuration
   keycloak: {
-    keycloakUrl: 'http://localhost:8080/auth',
+    keycloakUrl: 'http://localhost:8180/auth',
     realm: 'demo',
     clientId: 'angular-client',
-    "credentials": {
-    "secret": "8fca89d9-aee7-47e6-b5cc-935e774722fb"
-  } ,
+//    "credentials": {
+ //   "secret": "c6250098-662d-4e3e-8400-f241a88ac251"
+//  } ,
     redirectUri: 'http://localhost:4200'
   }
 
